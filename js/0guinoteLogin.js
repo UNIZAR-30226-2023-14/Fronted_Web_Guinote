2517e7399bfb60b77148bf5b9a98db7d5954e38b
Erika Rimacuna <816778@unizar.es>
2023-02-23 01:14:27 +0100

    incioSesionYRegistro

--- js/0guinoteLogin.js
+++ js/0guinoteLogin.js
@@ -3,24 +3,46 @@ const registerButton = document.getElementById('botonRegistro')
 
 const divBotonesInciales = document.getElementById('botonesIniciales')
 const divInicioSesion = document.getElementById('inicioSesion')
-
+const divRegistro = document.getElementById('registro')
 
 const username = document.getElementById('username')
 const password = document.getElementById('password')
-const button = document.getElementById('button')
+const buttonLogin = document.getElementById('button1')
+
+const usernameReg = document.getElementById('usernameReg')
+const passwordReg = document.getElementById('passwordReg')
+const passwordReg2 = document.getElementById('passwordReg2')
+const buttonSingIn = document.getElementById('button2')
 
 divInicioSesion.style.display = "none"
+divRegistro.style.display = "none"
 
 loginButton.addEventListener("click", function() {
     divBotonesInciales.style.display = "none";
     divInicioSesion.style.display = "flex";
   });
 
-button.addEventListener('click', (e) => {
+registerButton.addEventListener("click", function() {
+    divBotonesInciales.style.display = "none";
+    divRegistro.style.display = "flex";
+  });
+
+buttonLogin.addEventListener('click', (e) => {
     e.preventDefault()
     const data = {
         username: username.value,
         password: password.value
+    }
+
+    console.log(data)
+})
+
+buttonSingIn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const data = {
+        username: usernameReg.value,
+        password: passwordReg.value,
+        password2: passwordReg2.value
     }
 
     console.log(data)
