c6ecabd42f6174b144622d8ca4fa0531f985b824
Erika Rimacuna <816778@unizar.es>
2023-03-11 00:38:41 +0100

    menu Interactivo

---
+++ js/1guinoteMenu.js
@@ -0,0 +1,10 @@
+const options = document.querySelectorAll(".options");
+const botonSupIzq = document.querySelectorAll(".botonsuperiorIzq");
+const botonSupDer = document.querySelectorAll(".botonsuperiorDer");
+
+options[1].style.display = "none";
+options[2].style.display = "none";
+options[3].style.display = "none";
+botonSupIzq[1].style.display = "none";
+botonSupIzq[1].style.marginLeft = '6%' ;
+botonSupDer[1].style.display = "none";
